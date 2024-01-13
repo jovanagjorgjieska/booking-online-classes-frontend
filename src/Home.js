15836945700876eb8368cd9028c46783e3600bfc
jovanagjorgjieska <jovana.gjorgjieska@students.finki.ukim.mk>
2024-01-13 19:00:26 +0100

    Added diferentiating between type of users on Home

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,31 @@
+import { useState, useEffect } from "react";
 import CourseList from "./CourseList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-    const {data: courses, isPending, error} = useFetch('http://localhost:8080/api/courses');
     const token = localStorage.getItem('jwtToken');
     const loggedUser = localStorage.getItem('user');
+    const {data: profile, fetchUserError, fetchUserIsPending} = useFetch('http://localhost:8080/api/auth/' + loggedUser);
+    const [isTeacher, setIsTeacher] = useState(false);
+
+    useEffect(() => {
+        if (profile?.userRole === 'TEACHER') {
+            setIsTeacher(true);
+        }
+    }, [profile]);
+
+    const apiUrl = isTeacher
+            ? 'http://localhost:8080/api/teachers/' + profile.userId + '/courses'
+            : 'http://localhost:8080/api/courses';
+
+    const {data: courses, isPending, error} = useFetch(apiUrl);
 
     return ( 
         <div className="home">
             {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
-            {courses && <CourseList courses={courses} title="All Courses"/>}
+            {courses && isTeacher && <CourseList courses={courses} title="Your Courses"/>}
+            {courses && !isTeacher && <CourseList courses={courses} title="All Courses"/>}
         </div>
     );
 }
